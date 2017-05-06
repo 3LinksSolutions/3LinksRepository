eb3d5985aa2f1ef2682b82a329f3b46d4810152f
jnhepets <3linkssolutions@gmail.com>
2017-05-06 22:23:13 +0800

    newly added files

---
+++ 3LinksSoul/3LinksSoul/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_3LinksSoul.MvcApplication" Language="C#" %>
---
+++ 3LinksSoul/3LinksSoul/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_3LinksSoul.Startup))]
+namespace _3LinksSoul
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
